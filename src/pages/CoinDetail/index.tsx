ed197f8d3263de8cd0b8228a73c7e21972b6e6e8
slogup-dale <dyddn304@gmail.com>
2022-11-28 18:49:29 +0900

    add: 상세 코인 데이터 추가

--- src/pages/CoinDetail/index.tsx
+++ src/pages/CoinDetail/index.tsx
@@ -1,18 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Typo from "../../components/atoms/Typo";
-import { Container, Header, Title } from "../Coin";
+import { CoinItem, Container, Header, Title } from "../Coin";
 
 function CoinDetail() {
   const { state } = useLocation();
   const [loading, setLoading] = useState(false);
+  const [coin, setCoin] = useState();
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      try {
+        const res = await fetch(
+          `https://api.coinpaprika.com/v1/coins/${state.name}`
+        );
+        const json = await res.json();
+        setCoin(json);
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false);
+      }
+    })();
+  }, [state.name]);
+
+  console.log("coin>>>>", coin);
 
   return (
     <Container>
       <Header>
         <Title>{state.name || "Loading..."}</Title>
       </Header>
-      {!loading ? "...loading" : "...loading"}
+      {!loading ? coin && <div>test</div> : "...loading"}
     </Container>
   );
 }
