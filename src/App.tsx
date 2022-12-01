3120eb85418bb92159e51461c4741b542426b6dc
slogup-dale <dyddn304@gmail.com>
2022-12-02 00:52:06 +0900

    add: recoil dark mode 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,15 @@ import { RouterProvider } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
 import routers from "./routers";
 import GlobalStyle from "./styles/GlobalStyle";
-import { darkTheme } from "./styles/theme";
+import { darkTheme, lightTheme } from "./styles/theme";
+import { useRecoilValue } from "recoil";
+import { isDarkAtom } from "./atoms/theme";
 
 function App() {
+  const isDark = useRecoilValue(isDarkAtom);
+
   return (
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={isDark ? lightTheme : darkTheme}>
       <GlobalStyle />
       <RouterProvider router={routers} />
       <ReactQueryDevtools initialIsOpen />
--- src/components/molecules/Header/index.tsx
+++ src/components/molecules/Header/index.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { useRecoilState } from "recoil";
+import { isDarkAtom } from "../../../atoms/theme";
 
 function Header() {
   const navigate = useNavigate();
+  const [isDarkMode, setIsDarkMode] = useRecoilState(isDarkAtom);
 
   const goToPage = (url: string) => {
     navigate(url);
@@ -17,8 +20,8 @@ function Header() {
         <li>
           <button onClick={() => goToPage("/coin")}>Coin</button>
         </li>
-        <button type="button" onClick={() => {}}>
-          DarkMode
+        <button type="button" onClick={() => setIsDarkMode((prev) => !prev)}>
+          {isDarkMode ? "DarkMode" : "LightMode"}
         </button>
       </ul>
     </header>
